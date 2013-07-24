0bbbc4f2ecd44317f18f34cb47fb8505634984ee
Dheeraj R <ramakrishna.dheeraj@gmail.com>
2013-07-24 08:11:49 +0000

    Changes to work for the URL

--- grader.js
+++ grader.js
@@ -20,11 +20,13 @@ References:
    - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
 */
 
+
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
